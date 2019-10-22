75b17d6c668e86621240295cc081182375860956
github username <github email address>
2019-10-22 18:02:08 -0400

    16 instruments clickable

--- frontend/js/instruments.js
+++ frontend/js/instruments.js
@@ -1,13 +1,12 @@
 const instruments = document.querySelector("ul#instruments");
 
 function switchInstrument(event) {
-  switch (event.target.id) {
-    case value:
-      break;
-
-    default:
-      break;
-  }
+    if (event.target.dataset.channel) {
+        if (instruments.querySelector(".instrument_selected")){
+            instruments.querySelector(".instrument_selected").className = "not-selected"
+        }
+        event.target.className = "instrument_selected"
+        cID = parseInt(event.target.dataset.channel);
 }
-
+}
 instruments.addEventListener("click", switchInstrument);
--- frontend/js/loadMIDIPlayer.js
+++ frontend/js/loadMIDIPlayer.js
@@ -1,23 +1,30 @@
 window.onload = function() {
-<<<<<<< HEAD
   const instrumentsName = [
-    "lead_2_sawtooth",
+    "acoustic_grand_piano",
+    "acoustic_guitar_nylon",
+    "acoustic_bass",
+    "electric_bass_pick",
     "electric_piano_2",
-    "rock_organ",
-    "percussive_organ"
+    "violin",
+    "tuba",
+    "clarinet",
+    "percussive_organ",
+    "orchestral_harp",
+    "ocarina",
+    "music_box",
+    "lead_1_square",
+    "lead_2_sawtooth",
+    "synth_bass_1",
+    "synth_drum"
   ];
-=======
-  const instrumentsName = ["violin", "tuba"];
->>>>>>> ff1b1496853250e43fa4836cb1d24ce70e7dd937
 
   MIDI.loadPlugin({
     soundfontUrl: "fatboy/",
     instruments: instrumentsName,
     onsuccess: function() {
-      MIDI.programChange(0, MIDI.GM.byName[instrumentsName[0]].number);
-      MIDI.programChange(1, MIDI.GM.byName[instrumentsName[1]].number);
-      // MIDI.programChange(2, MIDI.GM.byName[instrumentsName[2]].number);
-      // MIDI.programChange(3, MIDI.GM.byName[instrumentsName[3]].number);
+      for (let i = 0; i < instrumentsName.length; i++) {
+        MIDI.programChange(i, MIDI.GM.byName[instrumentsName[i]].number);
+      }
       startLooper();
     }
   });
