ceddf47cf2713a456c5fb19158011ba6ab9d03bb
James Sharpe <jameswrightsharpe@gmail.com>
2019-10-22 09:53:39 -0400

    added fetch rendering

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,4 +1,25 @@
-//going to be responsible for fetching data for the rest of the program
+const BASE_URL = "http://localhost:3000/api/v1";
+const LOOPS_URL = BASE_URL + `/loops`;
+const LOOP_URL = id => LOOPS_URL + "/" + id;
+const loops = document.querySelector("#loops");
+
+const content = {
+  method: "GET",
+  headers: {
+    Accept: "application/json"
+  },
+  body: JSON.stringify(mainBody)
+};
+fetch(LOOPS_URL, content)
+  .then(res => res.json())
+  .then(e => e.forEach(renderLoop));
+
+function renderLoop(loop) {
+  const li = document.createElement("li");
+  li.innerText = currentLoop.name;
+  loops.appendChild(li);
+}
+
 const currentLoop = {
   name: "untitled loop game",
   notes: [],
@@ -6,18 +27,3 @@ const currentLoop = {
   bars: 4,
   BPM: 65
 };
-
-const mainBody = {
-    //information to send to the server
-};
-const content = {
-    method: "METHOD",
-    headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-    },
-    body: JSON.stringify(mainBody)
-};
-fetch(URL, content)
-    .then(res => res.json())
-    .then(e => );
--- frontend/js/sidebar.js
+++ frontend/js/sidebar.js
@@ -1,6 +1,1 @@
 const createForm = document.querySelector("#create-form");
-
-const loops = document.querySelector("#loops");
-const li = document.createElement("li");
-li.innerText = currentLoop.name;
-loops.appendChild(li);
