5edb6353dfe3b7c3afd3c702f586a79017be34ca
github username <github email address>
2019-10-22 09:20:17 -0400

    added default values to backend

--- backend/app/models/loop.rb
+++ backend/app/models/loop.rb
@@ -6,5 +6,15 @@ class Loop < ApplicationRecord
     validates :beats_per_bar, :numericality => { :less_than_or_equal_to => 128 }
     validates :bars, :numericality => { :less_than_or_equal_to => 16 }
     validates :BPM, :numericality => { :less_than_or_equal_to => 160 }
+    after_initialize :default_values
+
+  private
+    def default_values
+      self.bars = 4
+      self.bpm = 92
+      self.beats_per_bar = 128
+      self.save
+    end
+end
 
 end
--- backend/app/models/note.rb
+++ backend/app/models/note.rb
@@ -1,3 +1,11 @@
 class Note < ApplicationRecord
     belongs_to :loop
+    after_initialize :default_values
+
+  private
+    def default_values
+      self.velocity ||= 63
+      self.volume ||= 127
+      self.save
+    end
 end
