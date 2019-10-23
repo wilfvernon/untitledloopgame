35a8f7dd5f47263742ecb27a27fa1450d5aefc34
github username <github email address>
2019-10-23 11:43:56 -0400

    stashin

--- frontend/js/instruments.js
+++ frontend/js/instruments.js
@@ -2,9 +2,12 @@ const instruments = document.querySelector("ul#instruments");
 
 function switchInstrument(event) {
     if (event.target.dataset.channel) {
-        if (instruments.querySelector(".instrument_selected")){
-            instruments.querySelector(".instrument_selected").className = "not-selected"
+        previousInstrument = instruments.querySelector(".instrument_selected")
+        if (previousInstrument){
+            previousInstrument.className = "not-selected"
+            previousInstrument.blur()
         }
+        document.getElementById(event.target.id).focus()
         event.target.className = "instrument_selected"
         cID = parseInt(event.target.dataset.channel);
 }
